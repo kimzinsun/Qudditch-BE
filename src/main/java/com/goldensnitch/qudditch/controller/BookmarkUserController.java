26945b9d5cef8e71357998ea36e9c67f12d3b967
1min <dnjsxmf@naver.com>
2024-03-12 13:17:14 +0900

    feat: 유저 제품북마크 기능추가(insert, select)

---
+++ src/main/java/com/goldensnitch/qudditch/controller/BookmarkUserController.java
@@ -0,0 +1,33 @@
+package com.goldensnitch.qudditch.controller;
+
+import com.goldensnitch.qudditch.dto.CustomerBookmarkProduct;
+import com.goldensnitch.qudditch.dto.fcm.FCMNotificationRequestDto;
+import com.goldensnitch.qudditch.service.BookmarkUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/bookmark/user")
+public class BookmarkUserController {
+
+    @Autowired
+    BookmarkUserService service;
+
+    @GetMapping("/products")
+    public List<CustomerBookmarkProduct> getProduct(int userCustomerId){
+        log.info("BookmarkUserController.getProduct: userCustomerId {}", userCustomerId);
+
+        return service.getProducts(userCustomerId);
+    }
+
+    @PostMapping("/product")
+    public String createProduct(@RequestBody CustomerBookmarkProduct dto) {
+        log.info("BookmarkUserController.createProduct: CustomerBookmarkProduct {}", dto);
+
+        return service.createProduct(dto) ? "SUCCESS" : "FAIL";
+    }
+}
---
+++ src/main/java/com/goldensnitch/qudditch/service/BookmarkUserService.java
@@ -0,0 +1,23 @@
+package com.goldensnitch.qudditch.service;
+
+import com.goldensnitch.qudditch.dto.CustomerBookmarkProduct;
+import com.goldensnitch.qudditch.mapper.BookmarkUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookmarkUserService {
+
+    @Autowired
+    BookmarkUserMapper mapper;
+
+    public List<CustomerBookmarkProduct> getProducts(int userCustomerId){
+        return mapper.selectProducts(userCustomerId);
+    }
+
+    public boolean createProduct(CustomerBookmarkProduct dto){
+        return mapper.insertProduct(dto) > 0;
+    }
+}
