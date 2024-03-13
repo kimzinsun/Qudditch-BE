e9fed11642bcf5131e7b0e461768a351b4cbff9e
Kim JinSeon <kwlstjs00@gmail.com>
2024-03-13 11:30:31 +0900

    feat: Manager

---
+++ src/main/java/com/goldensnitch/qudditch/controller/ManageController.java
@@ -0,0 +1,11 @@
+package com.goldensnitch.qudditch.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/manage")
+public class ManageController {
+}
---
+++ src/main/java/com/goldensnitch/qudditch/service/ManageService.java
@@ -0,0 +1,7 @@
+package com.goldensnitch.qudditch.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManageService {
+}
