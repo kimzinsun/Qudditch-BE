4a763d7d174b921ab3bc0fa4581e2d6d3fa99cdb
HanDaWoon <bookpia.han@gmail.com>
2024-03-14 21:04:04 +0900

    feat: AmazonRekognitionClient 빈 등록

---
+++ src/main/java/com/goldensnitch/qudditch/config/AwsConfig.java
@@ -0,0 +1,22 @@
+package com.goldensnitch.qudditch.config;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+    @Value("${aws.profile}")
+    private String profile;
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonRekognition rekognitionClient() {
+        return AmazonRekognitionClient.builder().withRegion(Regions.fromName(region)).withCredentials(new ProfileCredentialsProvider(profile)).build();
+    }
+}
