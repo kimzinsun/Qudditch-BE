7bb107a4441dfbea9ecc674a5a009680ed916592
Kim JinSeon <kwlstjs00@gmail.com>
2024-03-18 10:24:55 +0900

    fix: response entity

--- src/main/java/com/goldensnitch/qudditch/controller/ProductController.java
+++ src/main/java/com/goldensnitch/qudditch/controller/ProductController.java
@@ -6,7 +6,10 @@ import com.goldensnitch.qudditch.dto.Product;
 import com.goldensnitch.qudditch.dto.StoreStock;
 import com.goldensnitch.qudditch.service.CrawlingService;
 import com.goldensnitch.qudditch.service.ProductService;
+import com.sendgrid.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,9 +35,24 @@ public class ProductController {
     }
 
     @GetMapping("/find/{productName}")
-    public List<Product> selectProductByName(@PathVariable String productName) {
-        return productService.selectProductByName(productName);
+    public ResponseEntity<Map<String, Object>> selectProductByName(@PathVariable String productName) {
+        List<Product> productList = productService.selectProductByName(productName);
+        String status = productList.isEmpty() ? "fail" : "success";
+
+
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", status);
+
+        if (productList.isEmpty()) {
+            response.put("message", "해당 상품이 존재하지 않습니다.");
+        } else {
+            response.put("data", productList);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
 
     // selectProductByName로 먼저 이름을 검색해서 productId를 가져온 후에 selectStoreStockByProductId로 호출하는 방식으로 변경
 
@@ -45,7 +63,9 @@ public class ProductController {
         double currentWgs84X = 128.91231380719825;
         double currentWgs84Y = 37.79691574964818;
 
+
         int count = productService.cntStoreStockByProductId(productId);
+
         List<StoreStock> storeStockList = productService.selectStoreStockByProductId(productId, currentWgs84X, currentWgs84Y, paginationParam);
         Pagination pagination = new Pagination(count, paginationParam);
 
