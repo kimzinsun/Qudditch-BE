e0971fe70143eefdf2cdc594f0c820d64c630355
HanDaWoon <bookpia.han@gmail.com>
2024-03-18 18:19:10 +0900

    feat: kinesisClient 빈 등록

--- src/main/java/com/goldensnitch/qudditch/config/AwsConfig.java
+++ src/main/java/com/goldensnitch/qudditch/config/AwsConfig.java
@@ -2,6 +2,8 @@ package com.goldensnitch.qudditch.config;
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.kinesis.AmazonKinesis;
+import com.amazonaws.services.kinesis.AmazonKinesisClient;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +19,17 @@ public class AwsConfig {
 
     @Bean
     public AmazonRekognition rekognitionClient() {
-        return AmazonRekognitionClient.builder().withRegion(Regions.fromName(region)).withCredentials(new ProfileCredentialsProvider(profile)).build();
+        return AmazonRekognitionClient.builder()
+            .withRegion(Regions.fromName(region))
+            .withCredentials(new ProfileCredentialsProvider(profile))
+            .build();
+    }
+
+    @Bean
+    public AmazonKinesis kinesisClient() {
+        return AmazonKinesisClient.builder()
+            .withRegion(Regions.fromName(region))
+            .withCredentials(new ProfileCredentialsProvider(profile))
+            .build();
     }
 }
