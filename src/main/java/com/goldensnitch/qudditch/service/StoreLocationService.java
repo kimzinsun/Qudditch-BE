554b47bdfd785d495a4725b8cae5d5b8de6fa5b0
o220s <pjs47792332@gmail.com>
2024-03-19 10:47:50 +0900

    fix : location limit와 controller 수정

--- src/main/java/com/goldensnitch/qudditch/service/StoreLocationService.java
+++ src/main/java/com/goldensnitch/qudditch/service/StoreLocationService.java
@@ -6,9 +6,7 @@ import com.goldensnitch.qudditch.dto.StoreStockRes;
 import com.goldensnitch.qudditch.mapper.StoreLocationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,14 +20,7 @@ public class StoreLocationService {
         this.storeLocationMapper = storeLocationMapper;
     }
 
-    @GetMapping("")
-    public List<Store> getLocation(double currentWgs84X, double currentWgs84Y) {
-
-        Map<String, Object> params = new HashMap<>();
-
-        params.put("currentWgs84X", currentWgs84X);
-        params.put("currentWgs84Y", currentWgs84Y);
-
+    public List<Store> getLocation(Map<String, Object> params) {
         return storeLocationMapper.getLocation(params);
     }
 
