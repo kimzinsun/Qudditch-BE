42271c221ba34ba663d17f4576b041c8f4426d6e
HanDaWoon <bookpia.han@gmail.com>
2024-03-19 17:58:03 +0900

    feat: base64 디코더 생성

--- src/main/java/com/goldensnitch/qudditch/util/AwsUtil.java
+++ src/main/java/com/goldensnitch/qudditch/util/AwsUtil.java
@@ -1,7 +1,11 @@
 package com.goldensnitch.qudditch.util;
 
+import com.amazonaws.services.rekognition.model.GetFaceSearchResult;
 import com.amazonaws.services.rekognition.model.S3Object;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class AwsUtil {
@@ -10,5 +14,12 @@ public class AwsUtil {
         s3Object.setBucket(bucketName);
         s3Object.setName(key);
         return s3Object;
+    }
+
+    public String getFaceSearchResultFromBase64(String base64) {
+        // base64 decoding
+        String decoded = new String(java.util.Base64.getDecoder().decode(base64));
+
+        return decoded;
     }
 }
