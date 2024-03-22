305ce839e5787a937a4e349396fa778ec09b3dfd
1min <dnjsxmf@naver.com>
2024-03-22 15:07:45 +0900

    feat: ResponseEntity 반환하도록 변경하고 insert에서 미리 중복된 행이 없는지 검증하는 로직추가

--- src/main/java/com/goldensnitch/qudditch/controller/BookmarkUserController.java
+++ src/main/java/com/goldensnitch/qudditch/controller/BookmarkUserController.java
@@ -4,9 +4,12 @@ import com.goldensnitch.qudditch.dto.CustomerBookmarkProduct;
 import com.goldensnitch.qudditch.service.BookmarkUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -17,23 +20,53 @@ public class BookmarkUserController {
     BookmarkUserService service;
 
     @GetMapping("/products")
-    public List<CustomerBookmarkProduct> getProduct(int userCustomerId){
+    public ResponseEntity<Map<String, Object>> getProduct(int userCustomerId){
+        Map<String, Object> map = new HashMap<>();
+
         log.info("BookmarkUserController.getProduct: userCustomerId {}", userCustomerId);
 
-        return service.getProducts(userCustomerId);
+        List<CustomerBookmarkProduct> bookmarkList = service.getProducts(userCustomerId);
+
+        map.put("bookmarkList", bookmarkList);
+
+        if (bookmarkList.isEmpty()) {
+            map.put("message", "해당 유저의 북마크 목록이 없습니다.");
+        } else {
+            map.put("message", "SUCCESS");
+        }
+
+        return ResponseEntity.ok().body(map);
     }
 
     @PostMapping("/product")
-    public String createProduct(@RequestBody CustomerBookmarkProduct dto) {
+    public ResponseEntity<String> createProduct(@RequestBody CustomerBookmarkProduct dto) {
         log.info("BookmarkUserController.createProduct: CustomerBookmarkProduct {}", dto);
 
-        return service.createProduct(dto) ? "SUCCESS" : "FAIL";
+        String message = "FAIL";
+
+        if (service.isDuplicateProduct(dto)){
+            message = "이미 추가된 북마크가 있습니다.";
+        } else {
+            service.createProduct(dto);
+            message = "SUCCESS";
+        }
+
+        return ResponseEntity.ok().body(message);
     }
 
     @DeleteMapping("/product")
-    public String removeProduct(CustomerBookmarkProduct dto) {
+    public ResponseEntity<String> removeProduct(CustomerBookmarkProduct dto) {
         log.info("BookmarkUserController.removeProduct: CustomerBookmarkProduct {}", dto);
 
-        return service.deleteProduct(dto) ? "SUCCESS" : "FAIL";
+        String message = "FAIL";
+        boolean isSuccess = service.deleteProduct(dto);
+
+        if (isSuccess) {
+            message = "SUCCESS";
+        } else {
+            message = "북마크가 삭제되지 않았습니다.";
+        }
+
+        return ResponseEntity.ok().body(message);
     }
 }
