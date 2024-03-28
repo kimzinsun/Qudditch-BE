ba999b51e03054ba6144705109546788af77cf0a
Kim JinSeon <kwlstjs00@gmail.com>
2024-03-28 14:25:44 +0900

    fix: pagination param 변경

--- src/main/java/com/goldensnitch/qudditch/dto/PaginationParam.java
+++ src/main/java/com/goldensnitch/qudditch/dto/PaginationParam.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class PaginationParam {
     private Integer page = 1;
-    private Integer recordSize = 5;
-    private Integer pageSize = 3;
+    private Integer recordSize = 10;
+    private Integer pageSize = 5;
     private Integer offset = 0;
 //    private String keyword;
 
