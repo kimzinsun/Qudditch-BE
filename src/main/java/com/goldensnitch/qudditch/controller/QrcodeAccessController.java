2aea5ac4f87283b328736c5e4ead76654e7d1bc0
Kim JinSeon <kwlstjs00@gmail.com>
2024-03-29 10:31:27 +0900

    refactor: 주석, print 제거

--- src/main/java/com/goldensnitch/qudditch/controller/QrcodeAccessController.java
+++ src/main/java/com/goldensnitch/qudditch/controller/QrcodeAccessController.java
@@ -6,10 +6,11 @@ import com.goldensnitch.qudditch.service.ExtendedUserDetails;
 import com.goldensnitch.qudditch.service.QrcodeAccessService;
 import com.goldensnitch.qudditch.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
