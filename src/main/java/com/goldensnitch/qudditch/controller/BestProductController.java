7257d7fefe997bfc1f35db92173daf2ee3c55d16
shinminchan <althwkd18@naver.com>
2024-03-29 11:20:57 +0900

    refactor: ID 수정

--- src/main/java/com/goldensnitch/qudditch/controller/BestProductController.java
+++ src/main/java/com/goldensnitch/qudditch/controller/BestProductController.java
@@ -3,8 +3,10 @@ package com.goldensnitch.qudditch.controller;
 
 import com.goldensnitch.qudditch.dto.StoreStockReport;
 import com.goldensnitch.qudditch.service.BestProductService;
+import com.goldensnitch.qudditch.service.ExtendedUserDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +28,9 @@ public class BestProductController {
     }
 
     @GetMapping("/BestProduct")
-    public Map<String,Object> BestProduct(Integer storeId) {
+    public Map<String,Object> BestProduct(@AuthenticationPrincipal ExtendedUserDetails userDetails) {
 
+        int storeId = userDetails.getId();
         List<StoreStockReport> list = bestProductService.BestProduct(storeId);
 
         return Map.of(
