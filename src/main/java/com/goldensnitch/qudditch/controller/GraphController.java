71f0a13f8c2ef92568d4319f65f6418adbffc079
1min <dnjsxmf@naver.com>
2024-03-29 14:50:52 +0900

    feat: customerUserId를 jwt에서 가지고 오도록 변경, 컨트롤러 log.info 삭제

--- src/main/java/com/goldensnitch/qudditch/controller/GraphController.java
+++ src/main/java/com/goldensnitch/qudditch/controller/GraphController.java
@@ -3,9 +3,11 @@ package com.goldensnitch.qudditch.controller;
 import com.goldensnitch.qudditch.dto.graph.CategoryGraphDto;
 import com.goldensnitch.qudditch.dto.graph.SalesGraphDto;
 import com.goldensnitch.qudditch.dto.graph.VisitorGraphDto;
+import com.goldensnitch.qudditch.service.ExtendedUserDetails;
 import com.goldensnitch.qudditch.service.GraphService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -22,22 +24,22 @@ public class GraphController {
 
     @GetMapping("/sales")
     // @RequestParam
-    public SalesGraphDto getSalesGraph(int userStoreId, String yearMonth){
-        log.info("GraphController.getSalesGraph: userStoreId: {}, yearMonth: {}", userStoreId, yearMonth);
+    public SalesGraphDto getSalesGraph(@AuthenticationPrincipal ExtendedUserDetails userDetails, String yearMonth){
+        int userStoreId = userDetails.getId();
 
         return service.getSalesGraph(userStoreId, yearMonth);
     }
 
     @GetMapping("/category")
-    public CategoryGraphDto getCategoryGraph(int userStoreId, String yearMonth){
-        log.info("GraphController.getSalesGraph: userStoreId: {}, yearMonth: {}", userStoreId, yearMonth);
+    public CategoryGraphDto getCategoryGraph(@AuthenticationPrincipal ExtendedUserDetails userDetails, String yearMonth){
+        int userStoreId = userDetails.getId();
 
         return service.getCategoryGraph(userStoreId, yearMonth);
     }
 
     @GetMapping("/visitor")
-    public VisitorGraphDto getVisitorGraph(int userStoreId, String yearMonth){
-        log.info("GraphController.getVisitorGraph: userStoreId: {}, yearMonth: {}", userStoreId, yearMonth);
+    public VisitorGraphDto getVisitorGraph(@AuthenticationPrincipal ExtendedUserDetails userDetails, String yearMonth){
+        int userStoreId = userDetails.getId();
 
         return service.getVisitorGraph(userStoreId, yearMonth);
     }
