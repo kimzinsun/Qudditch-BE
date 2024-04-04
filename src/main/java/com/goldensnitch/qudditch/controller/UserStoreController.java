87f748337f70a57fb910e6a3058e63b04ec51b1c
wonlog <bko0108@naver.com>
2024-04-04 15:01:18 +0900

    feat 가까운 가게 선택, 매장명 리스트

--- src/main/java/com/goldensnitch/qudditch/controller/UserStoreController.java
+++ src/main/java/com/goldensnitch/qudditch/controller/UserStoreController.java
@@ -1,12 +1,16 @@
 package com.goldensnitch.qudditch.controller;
 
+import com.goldensnitch.qudditch.dto.Store;
 import com.goldensnitch.qudditch.service.UserStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/userstore")
@@ -26,6 +30,17 @@ public class UserStoreController {
             return ResponseEntity.ok().body("userStore ID: " + userStoreId);
         } else {
             return ResponseEntity.badRequest().body("Invalid store ID or store could not be selected.");
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchByStoreName(@RequestParam String name){
+        try{
+            List<Store> stores = userStoreService.searchByStoreName(name);
+            return ResponseEntity.ok(stores);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("매장 검색 중 오류가 발생했습니다.");
+
         }
     }
 }
