c8ec4212d05ee743619d0c73d42353696f1474fe
HanDaWoon <bookpia.han@gmail.com>
2024-04-15 15:25:25 +0900

    feat: 사업자 번호만 가져오는 기능 구현

--- src/main/java/com/goldensnitch/qudditch/dto/RegisterStoreRequest.java
+++ src/main/java/com/goldensnitch/qudditch/dto/RegisterStoreRequest.java
@@ -1,16 +1,15 @@
 package com.goldensnitch.qudditch.dto;
 
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
-public class RegisterStoreRequest  {
+public class RegisterStoreRequest {
     private String email;
     private String password;
     private String name;
     private Integer storeId;
     private String businessNumber;
-    private MultipartFile businessLicenseFile;
+//    private MultipartFile businessLicenseFile;
 
     // Getters and Setters생략가능
 }
